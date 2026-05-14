58b4171fbd62e3673380958b957e331868b6b458
krinalharichtech-stack <krinal.harichtech@gmail.com>
2026-05-14 16:31:20 +0530

    modify api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,20 @@
+// import axios, { InternalAxiosRequestConfig } from "axios";
+
+// export const api = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL,
+// });
+
+// // Attach token automatically
+// api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+//   const token = localStorage.getItem("token");
+//   console.log("token", token);
+//   if (token) {
+//     config.headers = config.headers ?? {};
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({
@@ -5,12 +22,27 @@ export const api = axios.create({
 });
 
 // Attach token automatically
-api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-  const token = localStorage.getItem("token");
-  console.log("token", token);
-  if (token) {
-    config.headers = config.headers ?? {};
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+
+    // ✅ Safe for Vercel / SSR
+    if (typeof window !== "undefined") {
+
+      const token = localStorage.getItem("token");
+
+      console.log("token", token);
+
+      if (token) {
+        config.headers = config.headers ?? {};
+
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
