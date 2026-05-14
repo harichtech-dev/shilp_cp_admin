8862ddb368d50d9fedba22010a5af9bd50d65d26
krinalharichtech-stack <krinal.harichtech@gmail.com>
2026-05-14 17:19:27 +0530

    change login code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  return redirect("/login");
+  return (
+     redirect("/login")
+  );
 }
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,3 +1,22 @@
+// "use client";
+
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+// export const useAuth = () => {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//       document.cookie = `token=${token}; path=/`;
+//     } else {
+//       router.push("/login");
+//     }
+//   }, [router]);
+// };
+
 "use client";
 
 import { useEffect } from "react";
@@ -7,11 +26,13 @@ export const useAuth = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    // CHECK TOKEN FROM COOKIE
+    const token = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("token="));
 
-    if (token) {
-      document.cookie = `token=${token}; path=/`;
-    } else {
+    // IF TOKEN NOT FOUND
+    if (!token) {
       router.push("/login");
     }
   }, [router]);
